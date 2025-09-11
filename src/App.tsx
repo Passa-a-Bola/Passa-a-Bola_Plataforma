6a5f44cccd53067b27df0afc722cee680c932f76
Giovanna-Sant <giovanna.ssant@gmail.com>
2025-09-11 10:23:28 -0300

    fix/pasta adicional

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Home from './pages/home/Home'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Home/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
