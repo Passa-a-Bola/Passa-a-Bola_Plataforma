31e7a7ebc0aa1308b5f63daf85b3da89fa606035
Giovanna-Sant <giovanna.ssant@gmail.com>
2025-09-12 19:46:28 -0300

    feat/header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,42 @@
-import Headerbg from "../../assets/header-backgorund.png"
+import Headerbg from "../../assets/header-background.png"
 import "./Header.css"
+import { useState } from "react";
 
-function Header(){
-    return (
-        <div className="header">
-          <img src={Headerbg} alt="Imagem de fundo do header, tom roxo com as meninas no meio"/>
+function Header(){  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="header">
+      <img src={Headerbg} alt="imagem de header menians do passa a bola fundo roxo" />
+      <nav>
+        <div className="navbar rosa-bg font-bebas-neue-sub flex justify-center items-center py-1">
+          {/*navbar desktop*/}
+          <div className="hidden md:flex space-x-16">
+            <a href="#" className="underline">Calendário</a>
+            <a href="#" className="underline">Notícias</a>
+            <a href="#" className="underline">Sobre nós</a>
+            <a href="#" className="underline">Perfil</a>
+          </div>
+
+          {/*hamburger mobile */}
+          <div className="md:hidden">
+            <button onClick={() => setIsOpen(!isOpen)}>
+              {isOpen ? "X" : "☰"}
+            </button>
+          </div>
         </div>
-    )
+
+        {/* menu mobile */}
+        {isOpen && (
+          <div className=" rosa-bg font-bebas-neue items-center md:hidden flex flex-col space-y-2 ">
+            <a href="#" className="underline">Calendário</a>
+            <a href="#" className="underline">Notícias</a>
+            <a href="#" className="underline">Sobre nós</a>
+            <a href="#" className="underline">Perfil</a>
+          </div>
+        )}
+      </nav>
+    </div>
+  );
 }
 
 export default Header
